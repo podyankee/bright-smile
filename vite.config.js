d46864f2d6ecde372788127b53a029fa9d40ec3d
Kostyantin <podyankee@gmail.com>
2024-09-20 05:20:48 +0300

    section banner added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite';
 import pugPlugin from 'vite-plugin-pug';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 const options = { pretty: true, reload: true };
 const locals = { name: 'Flavien' };
 
 export default defineConfig({
-	plugins: [pugPlugin(options, locals)],
+	plugins: [
+		pugPlugin(options, locals),
+		ViteImageOptimizer({
+			jpg: {
+				quality: 80,
+			},
+			png: {
+				quality: 80,
+			},
+			webp: {
+				quality: 80,
+			},
+			avif: {
+				quality: 60,
+			},
+		}),
+	],
 });
